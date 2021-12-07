4627b3c9a073785de9c80a9588d8a94f037bf998
bongdang <95209776+bongdang@users.noreply.github.com>
2021-12-07 15:55:36 +0900

    original

---
+++ gifraw/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+GIF raw reading Library.
+
+Gifraw is a python GIF reading library.
+It is made for adding some feature to PILLOW/imageio palette problem.
+
+by bongdang
+"""
+
+from .__consts__ import VERSION, DESCRIPTION, AUTHOR
+
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
+from .mainclass import GifRaw
+
+def __repr__():
+    return f"{DESCRIPTION} : Version ({VERSION}) : by {AUTHOR}"
